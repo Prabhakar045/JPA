d9cbb0be603d8f5e0a5cb3e2cb35275d8d22cd1c
Prabhakar B K <prabhukattimani18@gmail.com>
2023-10-22 17:43:21 +0530

    this is my first Adance codes

---
+++ GiriasImpl.java
@@ -0,0 +1,76 @@
+package com.Xworkz.GiriasApplication.impl;
+
+import com.Xworkz.GiriasApplication.GiriasApp.Girias;
+import com.Xworkz.GiriasApplication.dto.ProductDTO;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class GiriasImpl implements Girias {
+    @Override
+    public boolean addProduct(ProductDTO dto) {
+        // Create an EntityManagerFactory using the correct persistence unit name
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("com.pk");
+
+        // Create an EntityManager
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        // Begin a transaction
+        entityManager.getTransaction().begin();
+
+        // Persist the ProductDTO entity
+        entityManager.persist(dto);
+
+        // Commit the transaction
+        entityManager.getTransaction().commit();
+
+        // Close the EntityManager and EntityManagerFactory
+        entityManager.close();
+        entityManagerFactory.close();
+
+        return true;
+    }
+
+    @Override
+    public ProductDTO getProductInfoId(int id) {
+        return new Persistence().createEntityManagerFactory("com.pk").createEntityManager().find(ProductDTO.class, 1);
+
+    }
+
+    @Override
+    public void updatePriceByProductId(double updatedPrice, int productID) {
+
+        EntityManager entityManager=new Persistence().createEntityManagerFactory("com.pk").createEntityManager();
+
+        ProductDTO dto = entityManager.find(ProductDTO.class,productID);
+
+        if (dto != null){
+            dto.setPrice(updatedPrice);
+            entityManager.getTransaction().begin();
+            entityManager.merge(dto);
+            entityManager.getTransaction().commit();
+
+            System.out.println("Price of product is successfully updated");
+        }
+    }
+
+    @Override
+    public int deleteProcuctinfoById(int id) {
+
+        EntityManager entityManager=new Persistence().createEntityManagerFactory("com.pk").createEntityManager();
+
+        ProductDTO dto = entityManager.find(ProductDTO.class, 1);
+
+        if (dto != null){
+            entityManager.getTransaction().begin();
+            entityManager.remove(dto);
+            entityManager.getTransaction().commit();
+
+            System.out.println("Price of product is successfully deleted");
+        }
+        return id;
+    }
+
+
+}
