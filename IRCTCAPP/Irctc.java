6b1693b4875c2e5af95b101cdcc18891f1eb1b5c
Prabhakar B K <prabhukattimani18@gmail.com>
2023-10-24 09:11:45 +0530

    Irctc Application 2nd class

---
+++ IRCTCAPP/Irctc.java
@@ -0,0 +1,9 @@
+package com.Xworkz.IRCTCApplication.IRCTCAPP;
+
+import com.Xworkz.IRCTCApplication.dto.TravellerDto;
+
+public interface Irctc {
+
+    public boolean bookTicket(TravellerDto dto);
+
+}
